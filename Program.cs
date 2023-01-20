fa8d0d500c6ff813fcf0660d575eb748a698e778
Diego Maia <diegom.santos03@gmail.com>
2023-01-19 22:21:57 -0300

    (chore): program class configured with interactive menu

--- Program.cs
+++ Program.cs
@@ -1,39 +1,44 @@
 ﻿using vehicle_parking_system.Models;
 
-Console.WriteLine("============================");
-Console.WriteLine("   - Escolha uma opção -   |");
-Console.WriteLine("============================");
-Console.WriteLine(" 1 - Entrada de veículo    |");
-Console.WriteLine(" 2 - Pesquisar veículos    |");
-Console.WriteLine(" 3 - Pagamento             |");
-Console.WriteLine(" 4 - Encerrar              |");
-Console.WriteLine("============================");
-int escolhaMenu = int.Parse(Console.ReadLine());
-
 ParkingSystem parking = new();
+parking.Menu = true;
 
-switch (escolhaMenu)
+while (parking.Menu == true)
 {
-    case 1:
-        escolhaMenu = 1;
-        // Criar método para cadastar veículos
-        parking.CadastrarVeiculo();
-        break;
-    case 2:
-        escolhaMenu = 2;
-        // Criar método para pesquisar veículos
-        parking.PesquisarListaVeiculos();
-        break;
-    case 3:
-        escolhaMenu = 3;
-        // Criar método para pagar e liberar os veículos 
-        break;
-    case 4:
-        escolhaMenu = 5;
-        //Encerrar o sistema
-        break;
+    Console.WriteLine("============================");
+    Console.WriteLine("   - Escolha uma opção -   |");
+    Console.WriteLine("============================");
+    Console.WriteLine(" 1 - Entrada de veículo    |");
+    Console.WriteLine(" 2 - Pesquisar veículos    |");
+    Console.WriteLine(" 3 - Pagamento             |");
+    Console.WriteLine(" 4 - Encerrar              |");
+    Console.WriteLine("============================");
+    int escolhaMenu = int.Parse(Console.ReadLine());
 
-    default:
-        //Avisar que não aceita essa escolha
-        break;
+    switch (escolhaMenu)
+    {
+        case 1:
+            escolhaMenu = 1;
+            parking.CadastrarVeiculo();
+            break;
+
+        case 2:
+            escolhaMenu = 2;
+            parking.PesquisarListaVeiculos();
+            break;
+
+        case 3:
+            escolhaMenu = 3;
+            parking.Pagamento();
+            break;
+
+        case 4:
+            escolhaMenu = 5;
+            parking.EncerrarSistema(parking.Menu);
+            break;
+
+        default:
+            Console.WriteLine("Não existe essa opção no menu.");
+            break;
+    }
 }
